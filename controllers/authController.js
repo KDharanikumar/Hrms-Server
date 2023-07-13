c9e467b893486e075414987b4eb1649533b357bc
Dharanikumar K <kdharanstarflit@gmail.com>
2023-07-13 19:08:47 +0530

    Change

--- controllers/authController.js
+++ controllers/authController.js
@@ -186,10 +186,16 @@ exports.changePassword = async (req, res, next) => {
 // Update Profile - /api/update
 
 exports.updateProfile = async (req, res, next) => {
-	const newUserData = {
+	let newUserData = {
 		name: req.body.name,
 		email: req.body.email,
 	};
+
+	let avatar;
+	if (req.file) {
+		avatar = `${process.env.BACKEND_URL}/uploads/user/${req.file.originalname}`;
+		newUserData = { ...newUserData, avatar };
+	}
 
 	const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
 		new: true,
