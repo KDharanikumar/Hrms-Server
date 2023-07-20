d54bb0387aee70c0552fe589b3716bfb52e0bf7f
Dharanikumar K <kdharanstarflit@gmail.com>
2023-07-20 18:22:54 +0530

    change

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,8 @@ router.route("/update").put(isAuthenticatedUser, (upload.single("avatar"), updat
 // Admin Routes
 
 router.route("/admin/users").get(getAllUsers);
-router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getUser);
+// router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getUser);
+router.route("/admin/user/:id").get(getUser);
 router.route("/admin/user/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateUser);
 router.route("/admin/user/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
